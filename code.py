1d1d0016acde2cc833d1f0820f86d2422cae1f31
rajasamineedi <rajasamineedi@gmail.com>
2025-10-26 21:36:29 +0530

    added changes by gangadhar

--- code.py
+++ code.py
@@ -2,6 +2,3 @@ for i in range(5):
     if i<3:
         print("working")
     print("error: Something failing")
-
-git config --global user.name "rajasamineedi"
-git config --global user.email "rajasamineedi@gmail.com"
